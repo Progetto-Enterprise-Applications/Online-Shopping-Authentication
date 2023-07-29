a5d41ee71810e2f74cac544f94fa709fa7ff39fb
PierKnight <pier.altimari@libero.it>
2023-07-29 12:07:03 +0200

    Added functional registration page, missing style completely

--- src/main/java/com/enterpriseapplications/authenticationspring/controllers/view/LoginController.java
+++ src/main/java/com/enterpriseapplications/authenticationspring/controllers/view/LoginController.java
@@ -1,10 +1,19 @@
 package com.enterpriseapplications.authenticationspring.controllers.view;
 
+import com.enterpriseapplications.authenticationspring.dto.RegisterUser;
+import com.enterpriseapplications.authenticationspring.service.interfaces.LocalUserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@AllArgsConstructor
 public class LoginController  {
+
+    private final LocalUserService userService;
 
     @GetMapping("/login")
     public String login(){
@@ -14,6 +23,24 @@ public class LoginController  {
     @GetMapping("/registration")
     public String registration(){
         return "registration";
+    }
+
+    @PostMapping("/registerUser")
+    public String redirectPostToPost(@Valid RegisterUser registerUser, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors())
+            return "redirect:/registration?invalidRegister";
+
+        try{
+            this.userService.insertUser(registerUser);
+        }
+        catch (Exception exception)
+        {
+            return "redirect:/registration?error";
+        }
+
+
+        return "redirect:/login?createdAccount";
     }
 
 }
